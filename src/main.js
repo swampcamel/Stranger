79c5e3c0722f23794a6342f782e318d3d697ecfd
Dan Mace <dmacebeta@gmail.com>
2018-10-05 11:57:42 -0700

    add interface logic for displaying years left to live.

--- src/main.js
+++ src/main.js
@@ -15,5 +15,10 @@ $(document).ready(function() {
     $('#venus-age').text(userInfo.venusAge);
     $('#mars-age').text(userInfo.marsAge);
     $('#jupiter-age').text(userInfo.jupiterAge);
+    $('#earth-life').text(userInfo.earthLife + " Terran years to meat-suit termination.");
+    $('#mercury-life').text(userInfo.mercuryLife + " Mercurial years to meat-suit termination.");
+    $('#venus-life').text(userInfo.venusLife + " Venusian years to meat-suit termination.");
+    $('#mars-life').text(userInfo.marsLife + " Martian years to meat-suit termination.");
+    $('#jupiter-life').text(userInfo.jupiterLife + " Jovian years to meat-suit termination.");
   });
 });
