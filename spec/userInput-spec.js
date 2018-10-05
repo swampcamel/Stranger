7388cfd8d2f68b33a70461e1774adee3050cb945
Dan Mace <dmacebeta@gmail.com>
2018-10-05 11:26:43 -0700

    create formulas and tests for calculating years left to live on various planets

--- spec/userInput-spec.js
+++ spec/userInput-spec.js
@@ -2,6 +2,8 @@ import UserAge from './../src/AgeConverter.js';
 
 describe('userAge', function() {
 
+  // age tests
+
   it('should test if calculation of Terran Age to Mercurial Age is accurate', function() {
     let userInfo = new UserAge(20);
     expect(userInfo.mercuryAge).toEqual(parseFloat(4.80).toFixed(2));
@@ -21,4 +23,32 @@ describe('userAge', function() {
     let userInfo = new UserAge(20);
     expect(userInfo.jupiterAge).toEqual(parseFloat(237.20).toFixed(2));
   });
+
+  // years remaing tests
+
+    it('should test if calculation of years left on Earth is correct', function() {
+        let userInfo = new UserAge(20);
+        expect(userInfo.earthLife).toEqual(60);
+      });
+
+    it('should test if calculation of years left on Mercury is correct', function() {
+      let userInfo = new UserAge(20);
+      expect(userInfo.mercuryLife).toEqual(parseFloat(14.40).toFixed(2));
+    });
+
+    it('should test if calculation of years left on Venus is correct', function() {
+      let userInfo = new UserAge(20);
+      expect(userInfo.venusLife).toEqual(parseFloat(37.20).toFixed(2));
+    });
+
+    it('should test if calculation of years left on Mars is correct', function() {
+      let userInfo = new UserAge(20);
+      expect(userInfo.marsLife).toEqual(parseFloat(112.80).toFixed(2));
+    });
+
+    it('should test if calculation of years left on Jupiter is correct', function() {
+      let userInfo = new UserAge(20);
+      expect(userInfo.jupiterLife).toEqual(parseFloat(711.60).toFixed(2));
+    });
+
 });
